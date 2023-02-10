8c4e1982d04ce47cfb4fb595baf8b810a37e1574
Marie Akhramenko <marie.akhramenko@ecomcharge.com>
2023-02-10 15:13:22 +0100

    Add simple form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,18 @@ class HomeController < ApplicationController
   end
 
   def generate
+    @form = ShortLinkForm.new(permitted_params)
     binding.pry
+    if @form.valid?
+      # тут вся логика с редисом
+    else
+      render :index # тут подумать как показывать ошибки))
+    end
+  end
 
+  private
+
+  def permitted_params
+    params.permit(:link, :short_link, :expired_at)
   end
 end
--- app/forms/short_link_form.rb
+++ app/forms/short_link_form.rb
@@ -1,3 +1,30 @@
 class ShortLinkForm < OpenStruct
   include ActiveModel::Validations
+  extend ActiveModel::Naming
+
+  attr_accessor :link, :short_link, :expired_at
+
+  validates :link, presence: true # url
+  validates :short_link, length: { in: 5..10 }, allow_blank: true # может быть тут еще проверить есть ли такой ключ в редисе
+  validate :expired_at_is_in_future
+
+  def to_model
+    self
+  end
+
+  def to_key # ?
+    id
+  end
+
+  def persisted?
+    false
+  end
+
+  def expired_at_is_in_future
+    if expired_at
+      if expired_at.to_date <= Date.today
+        errors.add(:expired_at, "can't be in the past")
+      end
+    end
+  end
 end
